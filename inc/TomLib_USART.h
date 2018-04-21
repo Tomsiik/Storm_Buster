cf144f9d92c77e0c64211ab9d3522a0c847e0bc1
Tomsiik <37449127+Tomsiik@users.noreply.github.com>
2018-04-21 20:12:38 +0200

    změna USART knihovny

--- inc/TomLib_USART.h
+++ inc/TomLib_USART.h
@@ -10,11 +10,18 @@
 
 #include "stm32l4xx_ll_usart.h"
 
-
-
 typedef enum {
 	TRUE = 0x01,
 	FALSE = 0x00
 } USART_result;
+
+void TL_USART_putByte(USART_TypeDef *USARTx,char charr);
+void TL_USART_printf(USART_TypeDef *USARTx,char charr[]);
+void TL_USART_putBuffer(USART_TypeDef *USARTx,uint8_t data[],uint8_t size);
+USART_result TL_USART_Available(USART_TypeDef *USARTx);
+uint8_t TL_USART_ReadByte(USART_TypeDef *USARTx);
+
+
+
 
 #endif /* TOMLIB_USART_H_ */
--- src/TomLib_USART.c
+++ src/TomLib_USART.c
@@ -10,20 +10,24 @@
 #include "string.h"
 
 
-void TL_USART_putByte(char charr){
-	while(LL_USART_IsActiveFlag_TXE(USART2)==0){}
-	LL_USART_TransmitData8(USART2,charr);
+void TL_USART_putByte(USART_TypeDef *USARTx,char charr){
+	while(LL_USART_IsActiveFlag_TXE(USARTx)==0){}
+	LL_USART_TransmitData8(USARTx,charr);
 }
 
-void TL_USART_printf(char charr[]){
+void TL_USART_printf(USART_TypeDef *USARTx,char charr[]){
 	for(int i=0;i<strlen(charr);i++){
-		TL_USART_putByte(charr[i]);
+		TL_USART_putByte(USARTx,charr[i]);
 	}
-
+}
+void TL_USART_putBuffer(USART_TypeDef *USARTx,uint8_t data[],uint8_t size){
+	for(int i=0;i<size;i++){
+		TL_USART_putByte(USARTx,data[i]);
+	}
 }
 
-USART_result TL_USART_AVALAIBLE(){
-	if (LL_USART_IsActiveFlag_RXNE(USART2)==1){
+USART_result TL_USART_Available(USART_TypeDef *USARTx){
+	if (LL_USART_IsActiveFlag_RXNE(USARTx)==1){
 		return TRUE;
 	}
 	else{
@@ -32,7 +36,7 @@ USART_result TL_USART_AVALAIBLE(){
 
 }
 
-uint8_t TL_USART_ReadByte(){
-	return LL_USART_ReceiveData8(USART2);
+uint8_t TL_USART_ReadByte(USART_TypeDef *USARTx){
+	return LL_USART_ReceiveData8(USARTx);
 
 }
